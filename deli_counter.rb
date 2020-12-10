e4ce22b16c61d337dd2230165166eb0915195738
Ian Marshall <imarshall14@gmail.com>
2020-12-09 20:25:23 -0500

    commit tree 2f6ea840305934c8bfe2baba63196e9cda3cddd5

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require "pry"
+def line (deli)
+    line = []
+    if deli.count == 0
+        puts "The line is currently empty."
+    else
+        deli.each_with_index {|person, index| line.push "#{index + 1}. #{person}"}
+        puts "The line is currently: #{line.join(" ")}"
+    end 
+end
+
+def take_a_number (deli, person)
+    if deli.length == 0
+        deli << person
+        puts "Welcome, #{person}. You are number 1 in line."
+    else
+        deli << person
+        puts "Welcome, #{deli[-1]}. You are number #{deli.length} in line."
+    end
+end
+
+def now_serving(katz_deli)
+    if katz_deli.length == 0 
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+    end
+end
